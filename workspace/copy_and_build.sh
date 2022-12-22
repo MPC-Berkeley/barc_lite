53b050bacf9722e58988a766a0486a5f494e8c6a
Edward <zhu.edward445@gmail.com>
2022-12-22 11:57:27 -0800

    Fix typo in script

--- workspace/copy_and_build.sh
+++ workspace/copy_and_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Change this to where you mounted the directory containing your code
+export MOUNT_DIRECTORY="/project_code" 
+
 # Copy contents of mounted directory to 
-# /barc_lite/workspace/src/mpclab_controllers/mpclab_controlers/lib
-export MOUNT_DIRECTORY="/project_code" # Change this to where you mounted the directory containing your code
-cp -r ${MOUNT_DIRECTORY}/* /barc_lite/workspace/src/mpclab_controllers/mpclab_controllers/lib
+# /barc_lite/workspace/src/mpclab_controllers/mpclab_controlers/lib/mpclab_controllers
+cp -r ${MOUNT_DIRECTORY}/* /barc_lite/workspace/src/mpclab_controllers/mpclab_controllers/lib/mpclab_controllers
 
 # Rebuild ROS packages
 cd /barc_lite/workspace
